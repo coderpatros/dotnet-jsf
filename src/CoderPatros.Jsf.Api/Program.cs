5605ede244db6bfaf6d46d621b383fdc26d2fc05
Patrick Dwyer <email@patrickdwyer.au>
2026-02-27 18:56:19 +1000

    Add swagger API documentation
    Signed-off-by: Patrick Dwyer <email@patrickdwyer.au>

--- src/CoderPatros.Jsf.Api/Program.cs
+++ src/CoderPatros.Jsf.Api/Program.cs
@@ -1,11 +1,20 @@
 using CoderPatros.Jsf.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapKeyEndpoints();
 app.MapSignEndpoints();
 app.MapVerifyEndpoints();
+
+app.MapGet("/", () => Results.Redirect("/swagger")).ExcludeFromDescription();
 
 app.Run();
 
